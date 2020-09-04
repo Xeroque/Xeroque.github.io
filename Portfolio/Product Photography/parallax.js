40ea94a168ccb9700d83b9132a0364a22b7cf90b
Eduardo <64563427+Xeroque@users.noreply.github.com>
2020-09-04 21:37:49 +0100

    Update parallax.js

--- Portfolio/Product Photography/parallax.js
+++ Portfolio/Product Photography/parallax.js
@@ -3,10 +3,6 @@ COMPUTERS THESE DAYS HAVE MEMORY FOR THIS RIGHT?*/
 window.addEventListener("DOMContentLoaded", scrollLoop, false);
 
 var productphotography1 = document.querySelector('.productphotography1');
-var floatingbread = document.querySelector('.productphotography2');
-var floatingberries = document.querySelector('.productphotography3');
-var floatingcupcake = document.querySelector('.productphotography4');
-var floatingbox = document.querySelector('.productphotography5');
 var xScrollPosition
 var yScrollPosition
 
@@ -15,10 +11,6 @@ function scrollLoop(e){
     yScrollPosition = window.scrollY;
 
     setTranslate(0, yScrollPosition * 0.2, productphotography1);
-    setTranslate(0, yScrollPosition * 0.5, productphotography2);
-    setTranslate(0, yaScrollPosition * -0.5, fproductphotography3);
-    setTranslate(0, yScrollPosition * -0.5, productphotography4);
-    setTranslate(0, yScrollPosition * -0.6  ,productphotography5);
     requestAnimationFrame(scrollLoop)
 }
 
