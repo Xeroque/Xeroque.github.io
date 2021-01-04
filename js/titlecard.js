c6cd3aa1ab9c315e9584bfa441a9dd32bd219974
Xeroque <eduardo.daviddesousa@gmail.com>
2021-01-04 06:18:52 +0000

    big changes :)

--- js/titlecard.js
+++ js/titlecard.js
@@ -1,6 +1,6 @@
 const typedTextSpan = document.querySelector(".typed-text")
 const cursorSpan = document.querySelector(".cursor")
-const textArray = ["coding","design","architecture","eCommerce"];
+const textArray = ["coding","design","architecture","eCommerce","photography"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000;
