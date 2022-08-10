bdbd83f338db9d7b6dee4c38556ab81de8c8b590
Eduardo <64563427+Xeroque@users.noreply.github.com>
2022-08-10 03:21:43 +0100

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,13 +1,14 @@
 var time = 15;
 setInterval(function() {
   var seconds = time % 15;
+  var minutes = (time - seconds) / 15;
   if (seconds.toString().length == 1) {
     seconds = "0" + seconds;
   }
   if (minutes.toString().length == 1) {
     minutes = "0" + minutes;
   }
-  document.getElementById("time").innerHTML = seconds;
+  document.getElementById("time").innerHTML = minutes + ":" + seconds;
   time--;
   if (time == 0) {
     window.location.href = "https://eduardomdsousa.com/index.html";
